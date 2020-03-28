e3f7b9ac837464ae6cf8e8388baae47be6d77a28
Vito <Vito@LAPTOP-B1E0VK6V.station>
2020-03-28 16:53:16 +0100

    Introduzione di Prepared Statement

---
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -0,0 +1,65 @@
+package it.polito.tdp.esempioSQL.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LeggiBabs {
+	
+	public void run() {
+		String jdbcURL = "jdbc:mysql://localhost/babs?user=root&password=root";
+		
+		// Tutte le volte che scriviamo codici che implicano l'accesso a risorse esterne, 
+		// dobbiamo aspettarci delle eccezioni
+		try {
+			Connection conn = DriverManager.getConnection(jdbcURL); // Immaginimao la connection, 
+			// come un tubo che collega il mio programma al database
+			
+			// Metodo PERICOLOSO, e POCO EFFICIENTE (vedere lezione 11 primi 10 minuti
+			/*
+			Statement st = conn.createStatement(); // Immaginiamo lo statement come una navetta, 
+			// che contine le mie istruzioni, che viaggia all'interno della connection, da un capo all'altro
+			
+			String sql = "SELECT NAME FROM station WHERE landmark='" + inserimentoUtente + "'"; // PERICOLO
+			
+			ResultSet res = st.executeQuery(sql); // ResultSet contiene il modo per accedere al risultato
+			*/
+			
+			String sql = "SELECT NAME FROM station WHERE landmark = ?";
+			
+			PreparedStatement st = conn.prepareStatement(sql); // Immaginiamo lo statement come una navetta, 
+			// che contine le mie istruzioni, che viaggia all'interno della connection, da un capo all'altro
+			
+			st.setString(1, "Palo Alto");
+			
+			ResultSet res = st.executeQuery(); // ResultSet contiene il modo per accedere al risultato
+			
+			while(res.next()) {
+				String nomeStazione = res.getString("name");
+				
+				System.out.println(nomeStazione);
+			}
+			st.close(); // Se all'interno della stessa connessione devo far viaggiare piu' statement, 
+			// mi conviene chiudere  quelli di cui non ho piu' bisogno
+			
+			Statement st2 = conn.createStatement();
+			
+			conn.close(); // ricordarsi di chiudere sempre la connessione
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		// Pattern di programmazione di tipo FACTORY
+		// FACTORY: creazione dell'oggetto di una classe senza sapere il tipo della classe ( NON posso usaren NEW)
+		// Uso un metodo fornito da un'altra classe (il DriverManager), che internamente fara' new e riconoscera'
+		// il tipo di classe effettivo
+	}
+	
+	public static void main(String args[]) {
+		LeggiBabs babs = new LeggiBabs();
+		babs.run();
+	}
+}
